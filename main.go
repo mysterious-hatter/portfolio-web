11be61cbe72b859f13aca1327c3fe01dd5ac5ba3
Grigorii Eidelkind <gr.eydelkind@gmail.com>
2024-08-17 22:39:38 +0300

    Add html

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
+		http.ServeFile(w, r, "./static/index.html")
 	})
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "TEST")
