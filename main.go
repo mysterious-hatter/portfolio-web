2d508703efad7776689279476360b4fec568c706
Grigorii Eidelkind <gr.eydelkind@gmail.com>
2024-08-17 22:48:26 +0300

    fixed bug

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./static/index.html")
+		http.ServeFile(w, r, "index.html")
 	})
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "TEST")
